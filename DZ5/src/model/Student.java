fbee10c053da592bd6b0e797555fa50f7f9f6fce
asd <sksd@mail.ru>
2024-09-13 21:21:03 +0300

    prepare for DZ5

---
+++ DZ5/src/model/Student.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.List;
+
+public class Student extends User{
+    private  int studentId;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+
+
+    public Student(String firstName, String lastName, String middleName, int studentId) {
+        super(firstName, lastName, middleName);
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                '}';
+    }
+}
