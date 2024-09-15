e2f22f0e64a1d43b2ac5080faca3162905056f80
ADSorokin <alexsksd@gmail.com>
2024-09-15 22:38:48 +0300

    Update Controller.java

--- DZ7/src/controller/Controller.java
+++ DZ7/src/controller/Controller.java
@@ -12,20 +12,20 @@ public class Controller {
 
     public void addComplexNum(ComplexNumber num1, ComplexNumber num2) {
         ComplexNumber resultAdd = service.calculate(new Addition(), num1, num2);
-        view.outOnConsole("сложения : ",resultAdd);
+        view.outOnConsole("СЃР»РѕР¶РµРЅРёСЏ: ",resultAdd);
 
 
     }
 
     public void MultiplicationComplexNum(ComplexNumber num1, ComplexNumber num2){
         ComplexNumber resultMul = service.calculate(new Multiplication(), num1, num2);
-        view.outOnConsole("умножения : ",resultMul);
+        view.outOnConsole("СѓРјРЅРѕР¶РµРЅРёСЏ : ",resultMul);
     }
 
 
     public void DivisionComplexNum(ComplexNumber num1, ComplexNumber num2){
         ComplexNumber resultDiv = service.calculate(new Division(), num1, num2);
-        view.outOnConsole("деления : ",resultDiv);
+        view.outOnConsole("РґРµР»РµРЅРёСЏ : ",resultDiv);
 
     }
 
